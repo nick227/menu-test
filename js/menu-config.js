b136511ba252f27adcfc8ebf5568d574d89faed0
Nicholas Rios <nicholas.jay.rios@gmail.com>
2023-07-11 10:51:25 -0500

    cleanup

--- js/menu-config.js
+++ js/menu-config.js
@@ -1,98 +1,250 @@
-
-const commonButtons = {
-  "add": {
-    "text": "add",
-    "id": "add",
-    iconClass: 'fa-solid fa-plus',
-    "children": [
-      { "text": "image", "id": "imageField" },
-      { "text": "video", "id": "videoField" },
-      { "text": "field", "id": "fieldField" },
-      { "text": "text", "id": "textField" },
-      { "text": "widget", "id": "widgetField" }]
+const allButtons = {
+  add: {
+    text: "add",
+    id: "add",
+    iconClass: "fa-solid fa-plus",
+    children: [
+      { text: "image", id: "addImage" },
+      { text: "video", id: "addVideo" },
+      { text: "field", id: "addField" },
+      { text: "text", id: "addText" },
+    ],
   },
-  "duplicate": {
-    "text": "duplicate",
-    "id": "duplicate",
-    "children": [
-      { "text": "to all sections", "id": "duplicateSections" },
-      { "text": "here once", "id": "duplicateOnce" }]
+  duplicate: {
+    text: "duplicate",
+    id: "duplicate",
+    children: [
+      { text: "to all sections", id: "duplicateSections" },
+      { text: "here once", id: "duplicateOnce" },
+    ],
   },
-  "options": {
-    "text": "options",
-    "id": "options",
-    "children": [
-      { "text": "hide", "id": "hide" },
-      { "text": "disable", "id": "disable" },
-      { "text": "advanced", "id": "advanced" }
-      ]
+  options: {
+    text: "options",
+    id: "options",
+    children: [
+      { text: "hide", id: "hide" },
+      { text: "disable", id: "disable" },
+      { text: "advanced", id: "advanced" },
+    ],
   },
-  "imgStyle": {
-    "text": "style",
-    "id": "imgStyle",
-    "children": [
-      { "text": "shape", "id": "imgShape" },
-      { "text": "filter", "id": "imgFilter" },
-      { "text": "theme", "id": "imgTheme" }
+  imgStyle: {
+    text: "style",
+    id: "imgStyle",
+    children: [
+      { text: "shape", id: "imgShape" },
+      { text: "mask", id: "mask" },
+      { text: "theme", id: "theme" },
+    ],
+  },
+  style: {
+    text: "style",
+    id: "style",
+    children: [
+      { text: "background", id: "sectionBackground" },
+      { text: "flow", id: "sectionFlow" },
+      { text: "theme", id: "theme" },
+    ],
+  },
+  fieldStyle: {
+    text: "style",
+    id: "fieldStyle",
+    children: [
+      { text: "flow", id: "fieldFlow" },
+      { text: "theme", id: "theme" },
+    ],
+  },
+  mediaSrc: {
+    text: "source",
+    id: "mediaSrc",
+    html: `<input type="text" />`
+  },
+  imgShape: {
+    text: "shape",
+    id: "imgShape",
+  },
+  background: {
+    text: "background",
+    id: "background",
+  },
+  flow: {
+    text: "flow",
+    id: "flow",
+  },
+  autoplay: {
+    text: "autoplay",
+    id: "autoplay",
+    children: [
+      { text: "on", id: "autoPlayOn" },
+      { text: "off", id: "autoPlayOff" }
     ]
   },
-  "sectionStyle": {
-    "text": "style",
-    "id": "sectionStyle",
-    "children": [
-      { "text": "background", "id": "sectionBackground" },
-      { "text": "flow", "id": "sectionFlow" }
-      ]
-  }, 
-  "mediaSrc": {
-    "text": "source", 
-    "id": "mediaSrc"
-  }
+  change: {
+    text: "change",
+    id: "change",
+    children: [
+      { text: "image", id: "addImage" },
+      { text: "video", id: "addVideo" },
+      { text: "field", id: "addField" },
+      { text: "text", id: "addText" },
+    ],
+  },
+  theme: {
+    text: "theme",
+    id: "theme",
+  },
+  buttonStyle: {
+    text: "style",
+    id: "buttonStyle",
+    children: [
+      { text: "background", id: "background" },
+      { text: "shape", id: "shape" },
+      { text: "theme", id: "theme" },
+    ],
+  },
+  buttonOptions: {
+    text: "options",
+    id: "options",
+    children: [
+      { text: "rules", id: "rules" },
+    ],
+  },
+  minMaxInc: {
+    text: "min/max/inc",
+    id: "minMaxInc",
+  },
+  numSelectable: {
+    text: "selectable",
+    id: "numSelectable",
+    children: [
+      { text: "one", id: "one" },
+      { text: "all", id: "all" },
+    ],
+  },
+  titlePosition: {
+    text: "title",
+    id: "titlePosition",
+    children: [
+      { text: "top", id: "top" },
+      { text: "right", id: "right" },
+      { text: "bottom", id: "bottom" },
+      { text: "left", id: "left" },
+      { text: "center", id: "center" },
+    ],
+  },
+  align: {
+    text: "align",
+    id: "align",
+    children: [
+      { text: "top", id: "top" },
+      { text: "right", id: "right" },
+      { text: "bottom", bottom: "bottom" },
+      { text: "left", id: "left" },
+    ],
+  },
+  group: {
+    text: "group",
+    id: "group",
+  },
 };
 
 const global_menus = {
+  section: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
+  ],
   image: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.imgStyle,
-    commonButtons.mediaSrc,
-    commonButtons.options
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.imgStyle,
+    allButtons.mediaSrc,
+    allButtons.options,
   ],
   video: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.imgStyle,
-    commonButtons.mediaSrc,
-    commonButtons.options
-  ],
-  section: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.sectionStyle,
-    commonButtons.options
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.mediaSrc,
+    allButtons.autoplay,
+    allButtons.options,
   ],
   radio: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.sectionStyle,
-    commonButtons.options
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
   ],
   checkbox: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.sectionStyle,
-    commonButtons.options
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
+  ],
+  dropdown: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
   ],
   text: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.sectionStyle,
-    commonButtons.options
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
   ],
   textarea: [
-    commonButtons.add,
-    commonButtons.duplicate,
-    commonButtons.sectionStyle,
-    commonButtons.options
-  ]
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
+  ],
+  button: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.buttonStyle,
+    allButtons.buttonOptions,
+  ],
+  slider: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.theme,
+    allButtons.minMaxInc,
+  ],
+  filterList: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.options,
+  ],
+  graphical: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.change,
+    allButtons.style,
+    allButtons.titlePosition,
+    allButtons.numSelectable,
+    allButtons.options,
+  ],
+  multiple: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.align,
+    allButtons.group,
+  ],
+  subform: [
+    allButtons.add,
+    allButtons.duplicate,
+    allButtons.options,
+  ],
 };
--- js/script.js
+++ js/script.js
@@ -3,31 +3,11 @@ function setupMenus() {
   addGlobalCloseListener(['.example', '.menu', '.submenu']);
 }
 
-function addGlobalCloseListener(selectors) {
-  document.addEventListener('click', (event) => {
-    const isMatch = selectors.some(selector => event.target.closest(selector));
-    if (!isMatch) closeAllMenus();
-  });
-}
-
-function handleExampleClick(event) {
-  closeAllMenus();
-  const key = event.currentTarget.dataset.key;
-  attachMenu(key, event.currentTarget);
-}
-
-function handleMenuButtonClick(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  const isSubmenu = event.currentTarget.closest('.submenu');
-  if (!isSubmenu) showSubmenu(event);
-}
-
 function createButton(button) {
   const buttonElm = document.createElement('button');
   buttonElm.title = button.text;
   buttonElm.dataset.buttonId = button.id;
-  buttonElm.onclick = handleMenuButtonClick;
+  buttonElm.addEventListener("click", handleMenuButtonClick);
   if (button.iconClass) buttonElm.appendChild(createIcon(button.iconClass));
   else buttonElm.textContent = button.text;
   return buttonElm;
@@ -47,6 +27,56 @@ function createSubmenu(parentId, buttons) {
   return submenuWrapper;
 }
 
+function attachMenu(key, parentContainer) {
+  const currentObj = global_menus[key];
+  const menuElm = document.createElement('div');
+  menuElm.className = 'menu';
+  currentObj.forEach(button => {
+    const buttonElm = createButton(button);
+    if (button.children) parentContainer.appendChild(createSubmenu(button.id, button.children));
+    else if (button.html) {
+      const submenuWrapper = document.createElement('div');
+      submenuWrapper.className = 'submenu';
+      submenuWrapper.dataset.parentId = button.id;
+      submenuWrapper.innerHTML = button.html;
+      parentContainer.appendChild(submenuWrapper);
+    }
+    menuElm.appendChild(buttonElm);
+  });
+  parentContainer.appendChild(menuElm);
+}
+
+function showSubmenu(event) {
+  document.querySelectorAll('.submenu').forEach(el => (el.style.display = 'none'));
+  const buttonId = event.currentTarget.dataset.buttonId;
+  const submenu = document.querySelector(`.submenu[data-parent-id="${buttonId}"]`);
+  submenu.style.display = 'flex';
+}
+
+function handleExampleClick(event) {
+  closeAllMenus();
+  const key = event.currentTarget.dataset.key;
+  attachMenu(key, event.currentTarget);
+}
+
+function handleMenuButtonClick(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const isSubmenu = event.currentTarget.closest('.submenu');
+  if (!isSubmenu) showSubmenu(event);
+}
+
+function addGlobalCloseListener(selectors) {
+  document.addEventListener('click', (event) => {
+    const isMatch = selectors.some(selector => event.target.closest(selector));
+    if (!isMatch) closeAllMenus();
+  });
+}
+
+function closeAllMenus() {
+  document.querySelectorAll('.menu, .submenu').forEach(el => el.remove());
+}
+
 function attachExamples() {
   Object.keys(global_menus).forEach(key => attachExample(key));
 }
@@ -58,29 +88,6 @@ function attachExample(key) {
   elm.innerHTML = key;
   document.body.appendChild(elm);
   elm.addEventListener('click', handleExampleClick);
-}
-
-function attachMenu(key, parentContainer) {
-  const currentObj = global_menus[key];
-  const menuElm = document.createElement('div');
-  menuElm.className = 'menu';
-  currentObj.forEach(button => {
-    const buttonElm = createButton(button);
-    if (button.children) parentContainer.appendChild(createSubmenu(button.id, button.children));
-    menuElm.appendChild(buttonElm);
-  });
-  parentContainer.appendChild(menuElm);
-}
-
-function showSubmenu(event) {
-  document.querySelectorAll('.submenu').forEach(el => (el.style.display = 'none'));
-  const buttonId = event.currentTarget.dataset.buttonId;
-  const submenu = document.querySelector(`.submenu[data-parent-id="${buttonId}"]`);
-  submenu.style.display = 'block';
-}
-
-function closeAllMenus() {
-  document.querySelectorAll('.menu, .submenu').forEach(el => el.remove());
 }
 
 document.addEventListener('DOMContentLoaded', setupMenus);
